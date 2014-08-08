a19e63852613b5052520e0ab0a2d27ba1cd5a7f9
degoldschmidt <goldschmidt@physik3.gwdg.de>
2014-08-08 19:49:34 +0200

    f2

--- SORN.h
+++ SORN.h
@@ -26,41 +26,41 @@ public:
 	void test(mat data, int time);
 	void update(vec in);
 
-	vec x;						// state of reservoir excitatory units
-	vec R;						// recurrent drive
-	vec xp;		// pseudo-state of reservoir excitatory units (no input drive)
-	vec y;						// state of reservoir inhibitory units
-	vec the;					// excitatory thresholds
-	vec thi;					// inhibitory thresholds
-	double te_max;				// maximum excitatory thresholds
-	double ti_max;				// maximum inhibitory thresholds
+	vec x;				// state of reservoir excitatory units
+	vec R;				// recurrent drive
+	vec xp;				// pseudo-state of reservoir excitatory units (no input drive)
+	vec y;				// state of reservoir inhibitory units
+	vec the;			// excitatory thresholds
+	vec thi;			// inhibitory thresholds
+	double te_max;		// maximum excitatory thresholds
+	double ti_max;		// maximum inhibitory thresholds
 
-	mat H;						// reservoir state matrix
-	mat Hp;						// reservoir pseudo-state matrix
+	mat H;				// reservoir state matrix
+	mat Hp;				// reservoir pseudo-state matrix
 	mat dH;
 
-	mat W;						// excitatory weights of reservoir
-	mat Wie;					// excitatory-to-inhibitory weights of reservoir
-	mat Wei;					// inhibitory-to-excitatory weights of reservoir
-	mat Wu;						// input weights
-	mat init_W;					// initial reservoir weights
-	mat end_W;					// final reservoir weights
-	mat delta_W;				// changes of reservoir weights
-	uvec idx_W;					// indices of positive weights
+	mat W;				// excitatory weights of reservoir
+	mat Wie;			// excitatory-to-inhibitory weights of reservoir
+	mat Wei;			// inhibitory-to-excitatory weights of reservoir
+	mat Wu;				// input weights
+	mat init_W;			// initial reservoir weights
+	mat end_W;			// final reservoir weights
+	mat delta_W;		// changes of reservoir weights
+	uvec idx_W;			// indices of positive weights
 
-	int N;						// total number of reservoir units
-	int Ne;						// number of reservoir excitatory units (0.8*N)
-	int Ni;						// number of reservoir inhibitory units (0.2*N)
-	int Nu;	// number of reservoir units directly driven by one of the inputs
+	int N;				// total number of reservoir units
+	int Ne;				// number of reservoir excitatory units (0.8*N)
+	int Ni;				// number of reservoir inhibitory units (0.2*N)
+	int Nu;				// number of reservoir units directly driven by one of the inputs
 
-	double w_sp;				// sparseness of reservoir weight matrix
-	//double w_sR;				// spectral radius of reservoir weight matrix
+	double w_sp;		// sparseness of reservoir weight matrix
+	//double w_sR;		// spectral radius of reservoir weight matrix
 
-	double leak_rate;			// leaking rate (1 => "nothing is leaking")
+	double leak_rate;	// leaking rate (1 => "nothing is leaking")
 
-	double mu_STDP;				// learning rate STDP
-	double mu_IP;				// learning rate IP
-	vec target_rate;			// target rate for IP
+	double mu_STDP;		// learning rate STDP
+	double mu_IP;		// learning rate IP
+	vec target_rate;	// target rate for IP
 	bool STDP;
 	bool SN;
 	bool IP;
